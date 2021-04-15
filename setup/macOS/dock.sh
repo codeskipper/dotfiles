dc5cc1b80316afba554c41be670c51de9c61cc82
Martinus Verburg <mart.verburg@me.com>
2021-04-15 14:51:25 +0200

    Not brew instlling FileMaker Pro, Chrome, Firefox. No hiding Dock, added LaunchPad.

--- setup/macOS/dock.sh
+++ setup/macOS/dock.sh
@@ -5,13 +5,14 @@ echo "setup/dock.sh Setting up your Dock"
 # Set the icon size of Dock items to 35 pixels
 defaults write com.apple.dock tilesize -int 35
 
-# Set the dock to autohide
-defaults write com.apple.Dock autohide 1
+# Set the dock to NOT autohide
+defaults write com.apple.Dock autohide 0
 
 # Set the dock autohide delay to 0
-defaults write com.apple.Dock autohide-delay -float 0
+defaults write com.apple.Dock autohide-delay -float 0.5
 
 dockutil --no-restart --remove all
+dockutil --no-restart --add "/System/Applications/Launchpad.app"
 dockutil --no-restart --add "/System/Applications/Messages.app"
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
@@ -21,9 +22,7 @@ dockutil --no-restart --add "/Applications/Microsoft Teams.app"
 #dockutil --no-restart --add "/Applications/Sourcetree.app"
 [[ -e "/Applications/BBEdit.app" ]] && dockutil --no-restart --add "/Applications/BBEdit.app"
 [[ -e "/Applications/Visual Studio Code.app" ]] && dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-#dockutil --no-restart --add "/Applications/Franz.app"
-#dockutil --no-restart --add "/Applications/KeeWeb.app"
-#dockutil --no-restart --add "/Applications/Sketch.app"
+
 dockutil --no-restart --add "/Applications/Slack.app"
 #dockutil --no-restart --add "/System/Applications/Calendar.app"
 dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
